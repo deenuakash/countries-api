068d9fdf8f230b819c69c48b495feef6a06221ae
deenu <akashraja1108@gmail.com>
2024-02-21 00:20:06 +0530

    Initial project setup
    Removed unnecessary assets and files
    Added Tailwind css to project and configured Design language
    Updated Favicon

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Nunito Sans", "sans-serif"],
+    },
+    colors: {
+      "dark-blue": "hsl(209, 23%, 22%)",
+      "dark-bg": "hsl(207, 26%, 17%)",
+      "light-text": "hsl(200, 15%, 8%)",
+      "dark-gray": "hsl(0, 0%, 52%)",
+      "light-bg": "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
