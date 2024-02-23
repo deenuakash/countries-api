0c52016efae78b78be8e8178cf59b910c6f1cbf9
deenu <akashraja1108@gmail.com>
2024-02-24 02:20:23 +0530

    update missing themeContext file

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const themeContext = createContext();
+
+const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState("Light");
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "Light" ? "Dark" : "Light"));
+  return (
+    <themeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </themeContext.Provider>
+  );
+};
+
+const useTheme = () => {
+  console.log(themeContext);
+  return useContext(themeContext);
+};
+
+export { ThemeContextProvider, themeContext, useTheme };
